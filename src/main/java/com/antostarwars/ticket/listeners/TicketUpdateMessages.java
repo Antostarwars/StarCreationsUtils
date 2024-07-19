5e66f0960b1415fadd0094c854908e39ce8987bb
Antostarwars <derosaantonio587@gmail.com>
2024-07-20 00:13:48 +0200

    Better logs in tickets.

--- src/main/java/com/antostarwars/ticket/listeners/TicketUpdateMessages.java
+++ src/main/java/com/antostarwars/ticket/listeners/TicketUpdateMessages.java
@@ -14,9 +14,19 @@ import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 public class TicketUpdateMessages extends ListenerAdapter {
     @Inject
     private Bot instance;
+
+    private final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+            .appendPattern("EEE, dd MMM yyyy HH:mm:ss z")
+            .toFormatter()
+            .withZone(ZoneId.of("Europe/Rome"));
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (!event.isFromGuild()) return;
@@ -27,7 +37,7 @@ public class TicketUpdateMessages extends ListenerAdapter {
         if (!event.getGuild().equals(guild) || !event.getChannelType().equals(ChannelType.TEXT) || ticket == null) return;
         if (event.getAuthor().isBot()) return;
 
-        ticket.getMessagesTranscript().add(event.getAuthor().getName() + " [" + event.getMessage().getTimeCreated() + "] " + ": " + event.getMessage().getContentRaw() + "\n");
+        ticket.getMessagesTranscript().add(event.getAuthor().getName() + " [" + event.getMessage().getTimeCreated().format(formatter) + "] " + ": " + event.getMessage().getContentRaw() + "\n");
         if (!event.getMessage().getAttachments().isEmpty()) {
             for (Message.Attachment attachment : event.getMessage().getAttachments()) {
                 ticket.getMessagesTranscript().add(attachment.getProxyUrl() + "\n");
@@ -47,6 +57,6 @@ public class TicketUpdateMessages extends ListenerAdapter {
         if (!event.getGuild().equals(guild) || !event.getChannelType().equals(ChannelType.TEXT) || ticket == null) return;
         if (event.getAuthor().isBot()) return;
 
-        ticket.getMessagesTranscript().set(ticket.getMessagesTranscript().size() -1, event.getAuthor().getName() + " [" + event.getMessage().getTimeEdited() + "] " + ": " + event.getMessage().getContentRaw() + "\n");
+        ticket.getMessagesTranscript().set(ticket.getMessagesTranscript().size() -1, event.getAuthor().getName() + " [" + event.getMessage().getTimeEdited().format(formatter) + "] " + ": " + event.getMessage().getContentRaw() + " (EDITED)" + "\n");
     }
 }
